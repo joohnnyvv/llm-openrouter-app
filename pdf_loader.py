89202b29e241a700142c2031e5a66b3c152e959a
joohnnyvv <janekrembikowski@gmail.com>
2026-04-25 10:59:21 +0200

    ad pymupdf

---
+++ pdf_loader.py
@@ -0,0 +1,19 @@
+import fitz
+import os
+
+def load_pdf(file_path):
+    doc = fitz.open(file_path)
+    text = ""
+    for page in doc:
+        text += page.getText()
+    doc.close()
+    return text
+
+
+def load_documents_from_storage(folder_path):
+    documents = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".pdf"):
+            text = load_pdf(os.path.join(folder_path, filename))
+            documents.append({"filename": filename, "text": text})
+    return documents
